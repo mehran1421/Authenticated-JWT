bd87c27c13f02d94160979a19b9327f8c4115e5a
mehran1421 <m.kamrani1421@gmail.com>
2021-08-02 16:11:35 +0430

    custom django admin panel for authenticated

--- authmk/admin.py
+++ authmk/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from authmk.forms import UserChangeForm, UserCreationForm
+from authmk.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    readonly_fields = ['created_at', 'updated_at']
+    list_display = ('id', 'email', 'name', 'is_superuser', 'is_active')
+    list_filter = ('is_superuser', 'is_active')
+    fieldsets = (
+        ('Security information', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_superuser', 'is_active')}),
+        ('Important date', {'fields': ('created_at', 'updated_at')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'name')
+    ordering = ('-id',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
