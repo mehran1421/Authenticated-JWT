7b0d88e77e0f8c340c2d467925b2ef8bf142d287
mehran1421 <m.kamrani1421@gmail.com>
2021-08-02 15:44:01 +0430

    create Costom user

--- authmk/models.py
+++ authmk/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from .managers import UserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model for better management authenticated
+    """
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- config/settings/base.py
+++ config/settings/base.py
@@ -129,3 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Custom User Model
+AUTH_USER_MODEL = 'authmk.User'
