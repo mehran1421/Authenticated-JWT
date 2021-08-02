92c0ab09b907afa77cd933733b707a697b3be9e8
mehran1421 <m.kamrani1421@gmail.com>
2021-08-02 18:11:27 +0430

    use router in url for login user

--- authmk/urls.py
+++ authmk/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    LoginView
+)
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'auth', LoginView, basename='auth')
+
+# The API URLs are now determined automatically by the router.
+app_name = 'auth'
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
