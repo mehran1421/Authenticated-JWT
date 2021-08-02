9e2fc1f28cb06c4ed27f0e273ab5e7c672041db0
mehran1421 <m.kamrani1421@gmail.com>
2021-08-02 18:10:54 +0430

    write LoginView with viewsets

--- authmk/views.py
+++ authmk/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .tokens import get_refresh_token, get_access_token
+from .serializers import LoginEmailSerializer
+from rest_framework.viewsets import ViewSet
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
 
-# Create your views here.
+
+class LoginView(ViewSet):
+    """
+    for login user by post request and save object to Jwt model
+    """
+    serializer_class = LoginEmailSerializer
+
+    def create(self, request):
+        """
+        save access and refresh token in Jwt model with post request
+        :param request: for user Identification
+        :return: login user
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(username=serializer.validated_data.get('email'),
+                            password=serializer.validated_data.get('password'))
+        if not user:
+            return Response({"error": "Invalid email or password"}, status=400)
+
+        access = get_access_token({"user_id": user.id})
+        refresh = get_refresh_token()
+
+        return Response({"access": access, "refresh": refresh})
